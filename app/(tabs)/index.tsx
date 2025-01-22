957545ea9470552633c94841ce468f62b2744df5
cinesd <108825701+cinesd@users.noreply.github.com>
2025-01-22 11:13:08 +0100

    Refactor home screen to display a list of events
    - Replace the View component with FlatList to render a list of events from the events.json file
    - Import the EventListItems component to render each event item in the list
    - Update the Home component to use FlatList and EventListItems components
    - Remove unused imports and styles
    
    Closes #123

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,17 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { FlatList } from 'react-native';
+import EventListItems from '~/components/EventListItems';
+import events from '~/assets/events.json';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
+      <FlatList
+        data={events}
+        renderItem={({ item }) => <EventListItems event={item} />}
+        className="bg-white"
+      />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
