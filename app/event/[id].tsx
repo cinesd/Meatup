a06ff3a0202bb990c6f718f274de09b7f33f6bd2
cinesd <108825701+cinesd@users.noreply.github.com>
2025-01-22 14:48:13 +0100

    Refactor Event component to include a footer with RSVP button

--- app/event/[id].tsx
+++ app/event/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 import { useLocalSearchParams, Stack } from 'expo-router';
 import dayjs from 'dayjs';
 import events from '~/assets/events.json';
@@ -16,6 +16,14 @@ export default function Event() {
         {dayjs(event.datetime).format('ddd, DD MMM • HH:mm CEST')}
       </Text>
       <Text className="text-lg">{event.description}</Text>
+      {/* Footer */}
+      <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
+        <Text className="text-xl font-semibold">Free</Text>
+
+        <Pressable onPress={() => {}} className="rounded-md bg-red-500 p-5 px-8">
+          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
