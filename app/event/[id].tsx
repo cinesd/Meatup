4251eff0a9b9b722298a776457468a443f654797
cinesd <108825701+cinesd@users.noreply.github.com>
2025-01-29 14:31:26 +0100

    Refactor TabLayout component to fetch events from Supabase

--- app/event/[id].tsx
+++ app/event/[id].tsx
@@ -3,21 +3,49 @@ import { useLocalSearchParams, Stack } from 'expo-router';
 import dayjs from 'dayjs';
 import { useState, useEffect } from 'react';
 import { supabase } from '~/utils/supabase';
+import { useAuth } from '~/context/AuthProvider';
 
 export default function Event() {
   const { id } = useLocalSearchParams();
   const [event, setEvent] = useState<any>(null);
   const [loading, setLoading] = useState(true);
+  const [joined, setJoined] = useState<any>(null);
+
+  const { user } = useAuth();
 
   useEffect(() => {
     fetchEvent();
-  }, []);
+  }, [id]);
 
   const fetchEvent = async () => {
     setLoading(true);
     const { data, error } = await supabase.from('events').select('*').eq('id', id).single();
     setEvent(data);
     setLoading(false);
+
+    const { data: dataAttendance, error: errorAttendance } = await supabase
+      .from('attendance')
+      .select('*')
+      .eq('user_id', user?.id)
+      .eq('event_id', event.id)
+      .single();
+
+    setJoined(dataAttendance);
+
+    setLoading(false);
+  };
+
+  const joinEvent = async () => {
+    const { data, error } = await supabase
+      .from('attendance')
+      .insert({
+        event_id: event.id,
+        user_id: user?.id,
+      })
+      .select()
+      .single();
+
+    setJoined(data);
   };
 
   if (loading) return <ActivityIndicator size="large" color="#0000ff" />;
@@ -33,10 +61,13 @@ export default function Event() {
       {/* Footer */}
       <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
         <Text className="text-xl font-semibold">Free</Text>
-
-        <Pressable onPress={() => {}} className="rounded-md bg-red-500 p-5 px-8">
-          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
-        </Pressable>
+        {joined ? (
+          <Text className="text-lg font-bold text-green-500">You are joined</Text>
+        ) : (
+          <Pressable onPress={joinEvent} className="rounded-md bg-red-500 p-5 px-8">
+            <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+          </Pressable>
+        )}
       </View>
     </View>
   );
