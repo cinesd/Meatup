d004c0e346383342c5849dfe90f87ac7bbe3cf8d
cinesd <108825701+cinesd@users.noreply.github.com>
2025-01-31 06:44:41 +0100

    add the logic of displaying events' participants

--- app/event/[id]/attendance.tsx
+++ app/event/[id]/attendance.tsx
@@ -1,5 +1,32 @@
-import { Text } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { useState, useEffect } from 'react';
+import { Text, View, FlatList } from 'react-native';
+import { supabase } from '~/utils/supabase';
 
 export default function Attendance() {
-  return <Text>Attendance</Text>;
+  const { id } = useLocalSearchParams();
+  const [attendees, setAttendees] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchAttendance();
+  }, [id]);
+
+  const fetchAttendance = async () => {
+    const { data } = await supabase.from('attendance').select('*, profiles(*)').eq('event_id', id);
+
+    setAttendees(data ?? []);
+  };
+  return (
+    <View>
+      <Text>Attendance</Text>
+      <FlatList
+        data={attendees}
+        renderItem={({ item }) => (
+          <View className="p-3">
+            <Text className="text-lg font-bold">{item.profiles.full_name}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
