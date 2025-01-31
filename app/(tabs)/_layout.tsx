0856354346e9b8acd9ace44402e89013163031d2
cinesd <108825701+cinesd@users.noreply.github.com>
2025-01-31 07:28:04 +0100

    Ajout de l'écran de création d'événements et mise à jour des icônes des onglets

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,8 +19,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Events',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <HeaderButton />
@@ -29,10 +29,18 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="create"
+        options={{
+          title: 'New Event',
+          tabBarIcon: ({ color }) => <TabBarIcon name="plus" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
         }}
       />
     </Tabs>
---
+++ app/(tabs)/create.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function CreateEvent() {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  );
+}
